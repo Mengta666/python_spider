e4484430d7232973feb2615d8b9b660a58d72c6f
Mengta666 <qc18585280717@126.com>
2025-05-30 17:36:37 +0800

    Revert "天气预报小网站"
    This reverts commit 01e5f5fe

--- java_script/函数与对象.js
+++ java_script/函数与对象.js
@@ -1,3 +1,5 @@
+
+
 // 自执行函数写法1
 // a = (function (a,b){
 //     return a+b
@@ -18,7 +20,7 @@ let func = function (a,b){
     return a+b
 }
 console.log(func(1,2))
-// 在对象中使用（这个不是构造器构造的对象）
+// 在对象中使用
 var obj = {
     name : "张三",
     age : 18,
@@ -31,18 +33,3 @@ console.log(obj.func())
 
 
 // 函数传参时，传对象是引用传递，其他是值传递（对象名实际存储是对象地址）
-// 就算函数没有形参，js函数内部也有一个arguments(实际上是一个对象（不是构造器构造的对象，类似于py字典）)来接收传入的实参
-function func1(){
-    console.log(arguments)
-}
-func1(1,2,3,4,5)
-
-func2()
-// 函数提升（即使在func2()调用后面在声明定义函数也可以，因为声明会自动提升到代码顶部，
-// 与变量提升类似，但是变量提升只提升声明，不是提升定义的值），同名以最后一个函数为准
-function func2(){
-    console.log("这是原本的的func2")
-}
-function func2(){
-    console.log("这是新的func2")
-}
